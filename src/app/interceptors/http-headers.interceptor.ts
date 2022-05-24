f8ccbf12746d2542b6ce3ca70955bb89b7b6d852
Dev-Daljeet <sin10510@sheridancollege.ca>
2022-05-24 11:15:38 -0400

    Add http service class

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': '6c9f2779e5mshd3ade588351b759p1908f1jsn7a4fc0e9894c',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '173a78cc055c40879eb0fff8e2300885',
+      }
+    });
+    return next.handle(req);
+  }
+}
